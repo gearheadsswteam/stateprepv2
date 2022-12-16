15b3d4035a5bd99f4ceeff70dc75d8dffe93ac69
gearheadsswteam <hrishi.rane@gmail.com>
2022-12-15 23:59:15 -0600

    Basic auto stack working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousBlueRightParkNeutralStack.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousBlueRightParkNeutralStack.java
@@ -5,6 +5,7 @@ import static java.lang.Math.*;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -22,7 +23,7 @@ public class AutonomousBlueRightParkNeutralStack extends AbstractAutonomous {
     Pose2d[] parkPose = new Pose2d[]{new Pose2d(-11, 34, -PI / 2), new Pose2d(-35, 34, -PI / 2), new Pose2d(-59, 34, -PI / 2)};
     Pose2d knockPose = new Pose2d(-60, 11, 0);
     Pose2d backPose = new Pose2d(-50, 11, 0);
-    Pose2d intakePose = new Pose2d(-60, 11, 0);
+    Pose2d intakePose = new Pose2d(-64, 11, 0);
     ElapsedTime clock = new ElapsedTime();
     double time = 0;
     double dropTrajTime = 1000;
@@ -100,7 +101,7 @@ public class AutonomousBlueRightParkNeutralStack extends AbstractAutonomous {
 
         //Drop point to stack for the 2nd cone
         traj4 = robot.drive.trajectorySequenceBuilder(dropPose)
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(5, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
+                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(20, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
                 .setReversed(true)
                 .splineTo(backPose.vec(), backPose.getHeading() + PI)
                 .addDisplacementMarker(() -> {
@@ -169,6 +170,7 @@ public class AutonomousBlueRightParkNeutralStack extends AbstractAutonomous {
                     robot.extendArmProfile(time, armIn, 0);
                     robot.extendWristProfile(time, wristIn, 0);
                     retractTime = robot.restTime();
+                    retractDone = true;
                     dropTrajDone = false;
                 } else {//Three cycles for three cones done...go to park
                         robot.drive.followTrajectorySequenceAsync(traj5[runCase - 1]);
@@ -183,7 +185,14 @@ public class AutonomousBlueRightParkNeutralStack extends AbstractAutonomous {
             // Cone is inside
             //Go to drop position
             if (intakeTrajDone || (usingSensor && holderDetectionCount > holderMinCount)) {//cone is inside
-                robot.drive.followTrajectorySequenceAsync(traj3);
+                TrajectorySequence tempTraj = robot.drive.trajectorySequenceBuilder(robot.drive.getPoseEstimate())
+                        .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(20, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
+                        .lineTo(intakePose.vec())
+                        .addTemporalMarker(1, 0, () -> {
+                            robot.drive.followTrajectorySequenceAsync(traj3);
+                        })
+                        .build();
+                robot.drive.followTrajectorySequenceAsync(tempTraj);
                 robot.armProfile = forwardArmProfile1(time);
                         //.addExtendTrapezoidal(armMaxVel, armMaxAccel, liftHighClose[1], 0);
                 robot.wristProfile = forwardWristProfile1(time);
